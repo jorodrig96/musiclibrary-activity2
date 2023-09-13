86f6675b423b72cf4483d4585bc929387e6d32f7
Jose Rodriguez <joserodriguez@Joses-MBP-2.home>
2023-09-12 19:15:27 -0700

    routedom

--- src/components/ArtistView.js
+++ src/components/ArtistView.js
@@ -1,16 +1,49 @@
 // These components will be making separate API calls from the app
 // component to serve specific data about our artist
 import { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams, useNavigate, Link } from 'react-router-dom'
 
 function ArtistView() {
+    const navigate = useNavigate()    
     const { id } = useParams()
     const [ artistData, setArtistData ] = useState([])
 
+    useEffect(() => {
+        const API_URL= `http://localhost:4000/album/${id}`
+        const fetchData= async () => {
+            const response = await fetch(API_URL)
+            const resData = await response.json()
+            setArtistData(resData.results)
+        }
+        fetchData()
+    }, [id])
+
+    const justAlbums = artistData.filter(entry => entry.collectionType === 'Album')
+
+    const renderAlbum = justAlbums.map((album, i) => {
+        return (
+            <div key= {i}>
+                <Link to={`/album/${album.collectionId}`}>
+                    <p>{album.collectionName}</p>
+                </Link>
+            </div>
+        )
+    })
+
+    const navButtons = () => {
+        return (
+            <div>
+                <button onClick={() => navigate(-1)}>Back</button>
+                <button onClick={() => navigate('/')}>Home</button>
+            </div>
+        )
+    }
+
     return (
         <div>
-            <h2>The id passed was: {id}</h2>
-            <p>Artist Data Goes Here!</p>
+            {artistData.length ? <h2>{artistData[0].artistName}</h2> : <h2>Loading...</h2>}
+            {navButtons()}
+            {renderAlbum}
         </div>
     )
 }
